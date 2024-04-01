0d1fc2774bd806c52889bff34a405ea94c7b2c21
zav <evolutionzav@gmail.com>
2024-04-01 23:10:04 +0200

    my commit

--- src/javastarter/Program.java
+++ src/javastarter/Program.java
@@ -2,9 +2,15 @@ package javastarter;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println(sumOfNumber(15545));
+        // Задача №1.
+        System.out.println(sumOfNumber(15545)); // out 20
+        // Задача №2.
+        System.out.println(reversNumber(123456)); // out 654321
     }
 
+    /*
+     *   Завдання №1
+     */
     public static int sumOfNumber(int number) {
         int result = 0;
         int temp;
@@ -12,6 +18,21 @@ public class Program {
             temp = number % 10;
             number /= 10;
             result += temp;
+        }
+        return result;
+    }
+
+    /*
+     *  Завдання №2
+     */
+    public static int reversNumber(int number) {
+        int count = Integer.toString(number).length();
+        int result = 0;
+        int temp;
+        while (number > 0) {
+            temp = number % 10;
+            number /= 10;
+            result += temp * Math.pow(10, --count);
         }
         return result;
     }
