51ad7b5911788b6251df8cdfc8ae29e04db45e9d
zav <evolutionzav@gmail.com>
2024-04-01 22:21:42 +0200

    my commit

--- src/javastarter/Program.java
+++ src/javastarter/Program.java
@@ -2,6 +2,12 @@ package javastarter;
 
 public class Program {
     public static void main(String[] args) {
-        
+        System.out.println(sumOfNumber(145));
+    }
+
+    public static int sumOfNumber(int number) {
+        int result = 0;
+
+        return result;
     }
 }
