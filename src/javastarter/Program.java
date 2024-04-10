a1c8b6c630de6c5157f7311f8bb8fc4f9516084d
zav <evolutionzav@gmail.com>
2024-04-10 11:07:01 +0200

    my commit

--- src/javastarter/Program.java
+++ src/javastarter/Program.java
@@ -1,11 +1,15 @@
 package javastarter;
 
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
         // Задача №1.
         System.out.println(sumOfNumber(15545)); // out 20
         // Задача №2.
         System.out.println(reversNumber(123456)); // out 654321
+        // Задача №3.
+        System.out.println(calculateName("Roman")); // out 61
     }
 
     /*
@@ -33,6 +37,19 @@ public class Program {
             temp = number % 10;
             number /= 10;
             result += temp * Math.pow(10, --count);
+        }
+        return result;
+    }
+
+    /*
+     * Завдання №3
+     */
+    public static int calculateName(String name) {
+        int result = 0;
+        name = name.toLowerCase();
+        String s = "_abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < name.length(); i++) {
+            result += s.indexOf(name.charAt(i));
         }
         return result;
     }
